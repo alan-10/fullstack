8c6ad4b663f4603e2e6a137e486a09eb5581903c
alan silva <alanzinid@gmail.com>
2022-08-22 10:59:38 -0300

    fix: throw error when user is not found by id

--- src/modules/services/User/getUserByIdService/getUserByIdService.js
+++ src/modules/services/User/getUserByIdService/getUserByIdService.js
@@ -1,4 +1,6 @@
+const { request } = require("express");
 const { getUserRepositories } = require("../../../repositories");
+const { handleError } = require("../../../../shared/errors/handleError");
 
 const getUserByIdService = async ({
     user_id
@@ -17,7 +19,12 @@ const getUserByIdService = async ({
         user_id
     });
 
+
     const has_single_user = Array.isArray(users) && users.length > 0;
+
+    if(!has_single_user){
+        handleError("Missing user in database", 404);
+    }
 
     return {
         user: users,
